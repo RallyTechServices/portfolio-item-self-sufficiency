3d61d2d1c5f3caedeb6267cddaf063000813e842
Tyr Johanson <tyr@ca.com>
2018-03-13 18:54:46 +0000

    details view

--- src/javascript/TsSelfSufficiency.js
+++ src/javascript/TsSelfSufficiency.js
@@ -2,6 +2,8 @@
 Ext.define('TsSelfSufficiency', {
     extend: 'Ext.data.Model',
     fields: [
+        { name: 'InsideDescendentProjectStories', type: 'auto' },
+        { name: 'OutsideDescendentProjectStories', type: 'auto' },
         { name: 'TotalStoryCount', type: 'int', defaultValue: 0 },
         { name: 'TotalPoints', type: 'int', defaultValue: 0 },
         { name: 'InDescendentProjectStoryCount', type: 'int', defaultValue: 0 },
